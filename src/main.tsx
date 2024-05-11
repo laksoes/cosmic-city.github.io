5d7206955c9bc61040c33c6a04fbc20327dc1fb2
Nathaniel <nathaniel_ramirez@icloud.com>
2024-05-10 20:16:39 -0700

    use ldrs for loading pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,30 @@ import { StrictMode, Suspense } from "react";
 import Navbar from "./components/navbar";
 import { metadata } from "./metadata";
 import routes from "~react-pages";
+import { ring } from "ldrs";
 import "./index.css";
 
+ring.register();
+
 function App() {
-  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center gap-2">
+          <l-ring
+            size="25"
+            stroke="4"
+            bg-opacity="0.25"
+            speed="2"
+            color="white"
+          />
+          <p>Loading...</p>
+        </div>
+      }
+    >
+      {useRoutes(routes)}
+    </Suspense>
+  );
 }
 
 const app = createRoot(document.getElementById("root")!);
