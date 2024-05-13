792ada7508e4515e046fa399ef7e9c82e6a73b2a
Nathaniel <nathaniel_ramirez@icloud.com>
2024-05-12 20:17:25 -0700

    add better reload animation

--- src/components/randomizer.tsx
+++ src/components/randomizer.tsx
@@ -38,10 +38,16 @@ function Randomizer() {
     <>
       <p className="h-8">{random}</p>
       <button
-        className="inline-flex justify-center items-center"
+        className="group inline-flex justify-center items-center transition-all active:rotate-[-360deg] active:scale-75"
         onClick={() => randomize()}
       >
-        <svg viewBox="0 0 24 24" height={20} width={20} fill="#fff">
+        <svg
+          viewBox="0 0 24 24"
+          height={20}
+          width={20}
+          fill="#fff"
+          className="group-hover:fill-[#ddd] group-active:fill-[#888]"
+        >
           <path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8"></path>
         </svg>
       </button>
